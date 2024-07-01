bfb7e10221caa138647cbf2b961bfb30af354c4b
Fareed Ahmad <fareed77@gmail.com>
2024-07-01 17:36:54 -0400

    new files added

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,19 @@
+//main.js
+
+//window alert
+window.alert("hello world");
+
+//document.write 
+document.write("Helllo world from javascript");
+
+
+//creating variable
+var sen1="this is the beginning of the string";
+var sen2="this is the end of the string";
+
+//display 
+document.write(sen1+sen2);
+
+
+//expression
+document.write(33 * 4)
