1dd404c2596756fb227ad81c4fb4a84ad45bab1c
Fareed Ahmad <fareed77@gmail.com>
2024-07-01 21:33:01 -0400

    new files

---
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -0,0 +1,11 @@
+//main.js
+
+
+//function for button event
+function updateParagraph() {
+    // Step 4: Use document.getElementById to get the paragraph element by id
+    var paragraph = document.getElementById("message");
+
+    // Step 5: Update the content of the paragraph
+    paragraph.innerHTML = "The message has been updated!";
+}
